bb4ba6a5db23a055d7dc39c4aa6b7bf7bd5ae2ad
Your Name <email@example.com>
2017-12-29 15:04:51 +0800

    hahaha

--- 2.待开发代码区/ArithmeticClass.vb
+++ 2.待开发代码区/ArithmeticClass.vb
@@ -5,7 +5,7 @@ Imports Dll_ToolModule
 ''' 游戏算法类
 ''' </summary>
 Public Class ArithmeticClass
-    Dim haha As String = "github"
+    Dim haha As String = "github1"
     ''' <summary>
     ''' 用户字典
     ''' </summary>
